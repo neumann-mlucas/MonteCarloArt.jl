e41bd51dfab1201ec0b9339a573d19590d43d3c3
neumann-mlucas <neumann.m.lucas@gmail.com>
2025-04-28 22:59:13 -0300

    feat: improve MC algo

--- main.jl
+++ main.jl
@@ -24,33 +24,24 @@ function main()
         ENV["JULIA_DEBUG"] = Main
     end
 
-    # default colors
-    colors = parse_colors(args["colors"])
-
     # use command line options to define algorithm parameters
     args = merge(DefaultArgs, args)
     input, output = args["input"], args["output"]
 
+    @info "Loading input image: '$input'"
     if !args["color"]
-        @info "Loading input as grey image: '$input'"
         inp = MonteCarloArt.load_image(input)
-
-        @info "Running gray scale algorithm..."
-        out = MonteCarloArt.run(inp, args)
-
-        @info "Saving final output image to: '$output'"
-        out = Gray.(complement.(out))
-        save(output * ".png", out)
     else
-        @info "Loading input as color image: '$input'"
         inp = MonteCarloArt.load_color_image(input)
-
-        @info "Running RGB algorithm with colors $(hex.(colors))"
-        out = MonteCarloArt.run(inp, args)
-
-        @info "Saving final output image to: '$output'"
-        save(output * ".png", complement.(out))
     end
+
+    @info "Running MC algorithm"
+    out = MonteCarloArt.run(inp, args)
+
+    @info "Saving final output image to: '$output'"
+    out = complement.(convert.(RGB{N0f8}, out))
+    save(output * ".png", out)
+
     @info "Done"
 end
 
@@ -68,35 +59,17 @@ function parse_cmd()
         arg_type = String
         default = "output"
         "--steps"
-        help = "number of algorithm iterations"
+        help = "Number iterations (proporcitonal to the number of circles)"
         arg_type = Int
-        default = 600000
-        "--colors"
-        help = "HEX code of colors to use in RGB mode"
-        default = "#FF0000,#00FF00,#0000FF"
+        default = 200000
         "--color"
-        help = "RGB mode"
-        action = :store_true
-        "--gif"
-        help = "Save output as a GIF"
+        help = "Color mode"
         action = :store_true
         "--verbose"
-        help = "verbose mode"
+        help = "Verbose mode"
         action = :store_true
     end
     parse_args(parser)
-end
-
-function parse_colors(colors::String)::MonteCarloArt.Colors
-    to_color(c) = parse(RGB{N0f8}, c)
-    # default value for colors
-    rgb_colors = [RGB(1, 0, 0), RGB(0, 1, 0), RGB(0, 0, 1)]
-    try
-        rgb_colors = map(to_color, split(colors, ","))
-    catch e
-        @error "Unable to parse '$colors' $e"
-    end
-    return rgb_colors
 end
 
 end
--- montecarloart.jl
+++ montecarloart.jl
@@ -9,20 +9,18 @@ using Images
 using Logging
 using Statistics
 
-const Point = Tuple{Int64,Int64}
-const Image = Matrix{RGB{Float64}}
-const Colors = Vector{RGB{Float64}}
+const Image = Matrix{Lab{Float64}}
 
 const DefaultArgs = Dict{String,Any}
 
-const REL_RADIUS = 0.0025
+const N_PALLET = 64
+
+const REL_RADIUS = 0.0030
 const REL_STD_RADIUS = 0.8
 const MIN_RADIUS = 1.0
 
-# const BASE_ENERGY = N0f8(0.7)
-const BASE_ENERGY = 0.7
+const BASE_ENERGY = 0.1
 
-export aggreate_images
 export load_color_image
 export load_image
 export run
@@ -32,42 +30,52 @@ function load_image(image_path::String)::Image
     @assert isfile(image_path)
     img = Images.load(image_path)
     # Convert the Image to gray scale
-    complement.(RGB{Float64}.(Gray.(img)))
+    gray_img = complement.(Gray.(img))
+    convert.(Lab{Float64}, gray_img)
 end
 
 function load_color_image(image_path::String)::Image
     # Read the image and convert it to an array
     @assert isfile(image_path)
     img = Images.load(image_path)
-    complement.(RGB{Float64}.(img))
+    # Convert the Image to Lab color space
+    convert.(Lab{Float64}, complement.(img))
 end
 
 function run(inp::Image, args::Dict=DefaultArgs)::Image
-    @debug "Starting algorithm..."
+    @debug "Entering algorithm subroutine"
     h, w = size(inp)
-    out = zeros(RGB{Float64}, h, w)
+    out = zeros(Lab{Float64}, h, w)
+    penalty = zeros(Float64, h, w)
 
-    accept, mc_accept, misses  = 0, 0, 0
+    @info "Generatiing Image Color Pallet"
+    pallet = get_pallet(inp)
+
+    @info "Starting algorithm iterative steps"
+    accept, mc_accept, misses = 0, 0, 0
     for step in 1:args["steps"]
         @debug "Step: $step"
-        point = (rand(1:h), rand(1:w))
+        point = get_center(h, w)
         radius = get_radius(h, w)
 
         @debug "Creating candidate circle at: $point with radius: $radius"
         points = gen_circle_points((h, w), point, radius)
         pixels = getindex.(Ref(inp), points)
 
-        color = mean_color_lab(pixels)
-        energy = Gray{Float64}(color)
+        @debug "Picking a color in the pallet to use"
+        avg = mean_color(pixels)
+        idx = argmin([color_distance(c, avg) for c in pallet])
+        color = pallet[idx]
 
-        @debug "Running Monte Carlo Criteria to Draw the circle..."
+        @debug "Running Monte Carlo like Criteria to Draw the circle"
+        energy = mean(penalty[p] for p in points)
         draw_circle = false
-        if energy > BASE_ENERGY
-            @debug "Accpect energy, higher than base: $energy"
+        if energy < BASE_ENERGY
+            @debug "Accpect energy, lower than base: $energy"
             draw_circle = true
             accept += 1
-        elseif energy > rand() # wrong criteria
-            @debug "Accpect energy using MC criteria: $energy"
+        elseif (energy / BASE_ENERGY) < rand()
+            @info "Accpect energy using MC criteria: $(energy / BASE_ENERGY)"
             draw_circle = true
             mc_accept += 1
         else
@@ -78,12 +86,13 @@ function run(inp::Image, args::Dict=DefaultArgs)::Image
         if draw_circle
             @debug "Drawing circle at: $point with radius: $radius"
             for i in points
-                out[i] = clamp01(out[i] + color * 0.5)
-                inp[i] = clamp01(inp[i] - color * 0.7)
+                out[i] = color
+                penalty[i] += 1
             end
         end
     end
-    to_pct(x) = 100 * round(x/args["steps"], digits=2)
+
+    to_pct(x) = 100 * round(x / args["steps"], digits=2)
     @info "Done with processing image"
     @info "  - accept:    $accept ($(to_pct(accept)) %)"
     @info "  - mc_accept: $mc_accept ($(to_pct(mc_accept)) %)"
@@ -95,6 +104,10 @@ function get_radius(height, width)::Int64
     radius = min(height, width) * REL_RADIUS
     std = radius * REL_STD_RADIUS
     floor(Int, abs(randn() * std + max(1.2, MIN_RADIUS)))
+end
+
+function get_center(height, width)::Tuple{Int64,Int64}
+    (rand(1:height), rand(1:width))
 end
 
 function gen_circle_points(size::Tuple{Int,Int}, center::Tuple{Int,Int}, radius::Int)
@@ -110,19 +123,24 @@ function gen_circle_points(size::Tuple{Int,Int}, center::Tuple{Int,Int}, radius:
     points
 end
 
-function color_distance_lab(c1::RGB, c2::RGB)::Float64
-    lab1 = convert(Lab, c1)
-    lab2 = convert(Lab, c2)
-    sqrt((lab1.l - lab2.l)^2 + (lab1.a - lab2.a)^2 + (lab1.b - lab2.b)^2)
+function get_pallet(img::Image)::Vector{Lab{Float64}}
+    # get pixels array
+    pixels = reshape(collect(channelview(img)), 3, :)
+    # clustering
+    result = kmeans(pixels, N_PALLET)
+    # convert back to Lab color space
+    [Lab{Float64}(c...) for c in eachcol(result.centers)]
 end
 
-function mean_color_lab(pixels::Vector{RGB{Float64}})
-    labs = convert.(Lab, pixels)
-    n = length(labs)
-    l = sum(c.l for c in labs) / n
-    a = sum(c.a for c in labs) / n
-    b = sum(c.b for c in labs) / n
-    convert(RGB, Lab(l, a, b))
+function color_distance(c1::Lab, c2::Lab)::Float64
+    sqrt((c1.l - c2.l)^2 + (c1.a - c2.a)^2 + (c1.b - c2.b)^2)
+end
+
+function mean_color(pixels::Vector{Lab{Float64}})::Lab{Float64}
+    l = mean(c.l for c in pixels)
+    a = mean(c.a for c in pixels)
+    b = mean(c.b for c in pixels)
+    Lab{Float64}(l, a, b)
 end
 
 end
